03b03338970173e38b1d820388cc3fd6a4454ed9
wood.wu <kilokingw@gmail.com>
2024-11-05 18:01:41 +0800

    up openday set (viewer/ttxc)

--- src/Pages/Front/Ar/Helper.js
+++ src/Pages/Front/Ar/Helper.js
@@ -1,26 +1,31 @@
 export const requestOptions = {
-    method : 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': process.env.REACT_APP_FULLBODY_TOKEN_KEY
-    }
-}
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: process.env.REACT_APP_FULLBODY_TOKEN_KEY,
+  },
+};
 
 export const requestOptions_ttxc = {
-    method : 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': process.env.REACT_APP_TTXC_TOKEN_KEY
-    }
-    
-}
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: process.env.REACT_APP_TTXC_TOKEN_KEY,
+  },
+};
 
+export const requestOptions_openday = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: process.env.REACT_APP_ONENDAY_TOKEN_KEY,
+  },
+};
 
 export const requestOptions_s168 = {
-    method : 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': process.env.REACT_APP_S168_TOKEN_KEY
-    }
-    
-}
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: process.env.REACT_APP_S168_TOKEN_KEY,
+  },
+};
